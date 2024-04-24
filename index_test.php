794232b25378b4f9cab64d5456f85ca4c8fc5b68
elmourabetnada <nadaelmourabet2002@gmail.com>
2024-04-24 23:41:20 +0100

    test php code

---
+++ index_test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemple PHP</title>
+</head>
+<body>
+
+<?php
+    // Affichage de la date et de l'heure actuelles
+    echo "Date et heure actuelles : " . date("Y-m-d H:i:s");
+?>
+
+</body>
+</html>
+
